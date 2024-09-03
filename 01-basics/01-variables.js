796eef2d8f9110c35c765225e2c6c80d3c1d4304
Mizan8111 <136497915+Mizan8111@users.noreply.github.com>
2024-09-03 13:06:15 +0000

    variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "mizan@github.com"
+var accountPassward = "12345"
+accountCity = "jaipur"
+
+//accountId = 2
+
+accountEmail = "hchsvhab@gmail.com"
+accountPassward = "67832765827"
+accountCity="bengaluru"
+console.log(accountId);
